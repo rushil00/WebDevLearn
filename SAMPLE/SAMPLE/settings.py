11a3f8bb2bc3044ee48300d189a7aba7e64656e9
rushil00 <bhatnagar.rushil@gmail.com>
2023-06-17 16:30:22 +0530

    New Commits

--- SAMPLE/SAMPLE/settings.py
+++ SAMPLE/SAMPLE/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'HELLO',
     'newyear',
-    'tasks'
+    'tasks',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- SAMPLE/tasks/urls.py
+++ SAMPLE/tasks/urls.py
@@ -1,5 +1,7 @@
 from . import views
 from django.urls import path
+app_name = "tasks"
 urlpatterns = [
-    path("" , views.index, name='index')
+    path("" , views.index, name='index'),
+    path("add" , views.add, name='add' ),
 ]
